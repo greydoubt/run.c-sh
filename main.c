6fda46a1d189ca4694f094cb760a0a9f5feb0130
λx . ophidia ereц grex⋆. 𐙚 ̊      .    أوفيديا إريكس جريكس   . <43443470+greydoubt@users.noreply.github.com>
2026-03-28 06:14:53 -0700

    Update main.c
    ruin.h

--- main.c
+++ main.c
@@ -1,8 +1,38 @@
 #include "run.h"
 
 
-int shared_var = 10;
+const int shared_var = 10;
+#include "ruin.h"
+
+void run_commands(Command *cmds) {
+    for (int i = 0; cmds[i].cmd != NULL; i++) {
+        system(cmds[i].cmd);
+    }
+}
 
 int main() {
-    return add(shared_var, 5);
+    chdir("lib");
+
+    Command lib_steps[] = {
+        {"cc -c -O *.c"},
+        {"ar r /lib/liby.a *.o"},
+        {"rm *.o"},
+        {NULL}
+    };
+
+    run_commands(lib_steps);
+
+    chdir("../source");
+
+    Command src_steps[] = {
+        {"cc -s -O y?.c"},
+        {"cmp a.out /usr/bin/yacc"},
+        {"cp a.out /usr/bin/yacc"},
+        {"rm a.out *.o"},
+        {NULL}
+    };
+
+    run_commands(src_steps);
+
+    return 0;
 }
